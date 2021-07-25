1428bda54c336dbc1b67390ab0dfb34a83b9b59e
Guillermo Mira Osuna <miraosuna@usal.es>
2021-07-25 11:48:59 +0200

    1

--- 8linked_lists/ft_lstlast.c
+++ 8linked_lists/ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: guilmira <guilmira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/28 14:51:30 by guilmira          #+#    #+#             */
-/*   Updated: 2021/07/16 16:20:11 by guilmira         ###   ########.fr       */
+/*   Updated: 2021/07/24 10:35:45 by guilmira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 /** PURPOSE : returns address of the last element.
  * 1. While the NEXT element is pointing somewhere (i.e. not NULL) \
- * LIST is updated and then return
+ * LIST is updated and then returned
  * Note than in the main program, list will not be updated,
  * the function will only return a new adress, but not replace it. */
 t_list	*ft_lstlast(t_list *lst)
