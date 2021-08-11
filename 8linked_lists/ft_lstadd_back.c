58405b1cb0e2bb0c34c4f8f5639a8d2c6a328a9b
Guillermo Mira Osuna <miraosuna@usal.es>
2021-08-11 09:32:24 +0200

    1

--- 8linked_lists/ft_lstadd_back.c
+++ 8linked_lists/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: guilmira <guilmira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/29 15:37:17 by guilmira          #+#    #+#             */
-/*   Updated: 2021/07/16 16:12:56 by guilmira         ###   ########.fr       */
+/*   Updated: 2021/08/11 09:32:05 by guilmira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 /** PURPOSE : adds new node to the enf of the list.
  * 1. Advances list to the last element.
- * 2. Inserts newer elemen at the back of the list. */
+ * 2. Inserts newer element at the back of the list. */
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*tmp;
