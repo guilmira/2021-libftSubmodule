f2c94b7082033840e64a8f6427433d4b12ade26b
Guillermo Mira Osuna <miraosuna@usal.es>
2021-10-07 14:10:28 +0200

    shutdown modificado

--- 11error_mgmt/ft_shutdown.c
+++ 11error_mgmt/ft_shutdown.c
@@ -6,15 +6,17 @@
 /*   By: guilmira <guilmira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 11:24:32 by guilmira          #+#    #+#             */
-/*   Updated: 2021/09/27 11:03:47 by guilmira         ###   ########.fr       */
+/*   Updated: 2021/10/07 09:45:10 by guilmira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/** PURPOSE : Output error with value 1, close the program. */
+/** PURPOSE : Output error with value 1, close the program.
+ * Exit signal is 0, to prevent message "make: *** [exe] Error 1"
+ * from appearing on screen. */
 void	ft_shutdown(void)
 {
 	ft_putstr_fd("Exiting program.\n", 1);
-	exit(1);
+	exit(0);
 }
